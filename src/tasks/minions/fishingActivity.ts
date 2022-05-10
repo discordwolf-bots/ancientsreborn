115f9aba36f08f25d9866e10d5452895bed42c52
Matthew Degnan <38664414+discordwolf-bots@users.noreply.github.com>
2022-05-10 23:59:51 +0100

    Fishing
    Removed excess fishing code

--- src/tasks/minions/fishingActivity.ts
+++ src/tasks/minions/fishingActivity.ts
@@ -1,50 +1,36 @@
-import { calcPercentOfNum } from 'e';
 import { Task } from 'klasa';
 import { Bank } from 'oldschooljs';
 
+import { baseRewardMultiplierSkilling, baseXpMultiplier } from '../../config';
 import Fishing from '../../lib/skilling/skills/fishing';
 import { SkillsEnum } from '../../lib/skilling/types';
-import { FishingActivityTaskOptions } from '../../lib/types/minions';
-import { anglerBoostPercent } from '../../lib/util';
+import { GatheringActivityTaskOptions } from '../../lib/types/minions';
 import { handleTripFinish } from '../../lib/util/handleTripFinish';
 
 export default class extends Task {
-	async run(data: FishingActivityTaskOptions) {
-		let { fishID, quantity, userID, channelID, duration } = data;
+	async run(data: GatheringActivityTaskOptions) {
+		const { resourceID, quantity, userID, channelID, duration } = data;
 		const user = await this.client.fetchUser(userID);
 
-		const fish = Fishing.Fishes.find(fish => fish.id === fishID)!;
+		const fish = Fishing.Fishes.find(Fish => Fish.id === resourceID)!;
 
-		let xpReceived = 0;
-		xpReceived = quantity * fish.xp;
-		let bonusXP = 0;
+		let xpReceived = quantity * fish.xp * baseXpMultiplier;
 
-		let xpRes = await user.addXP({
-			skillName: SkillsEnum.Fishing,
+		const xpRes = await user.addXP({
+			skillName: SkillsEnum.Mining,
 			amount: xpReceived,
 			duration
 		});
 
-		let str = `${user}, ${user.minionName} finished fishing ${quantity} ${fish.name}. ${xpRes}`;
-
-		let lootQuantity = 0;
-
 		let loot = new Bank({
-			[fish.id]: lootQuantity
+			[fish.id]: quantity * baseRewardMultiplierSkilling
 		});
 
-		const xpBonusPercent = anglerBoostPercent(user);
-		if (xpBonusPercent > 0) {
-			bonusXP += Math.ceil(calcPercentOfNum(xpBonusPercent, xpReceived));
-		}
+		let str = `${user}, ${user.minionName} finished fishing. ${xpRes}`;
 
-		if (bonusXP > 0) {
-			str += `\n\n**Bonus XP:** ${bonusXP.toLocaleString()}`;
-		}
+		str += `\nYou received ${loot}.`;
 
 		await user.addItemsToBank({ items: loot, collectionLog: true });
-
-		str += `\n\nYou received: ${loot}.`;
 
 		handleTripFinish(
 			this.client,
