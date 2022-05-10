68c93e2a3a2c44b48075ffb02f6c368afefd4306
Matthew Degnan <38664414+discordwolf-bots@users.noreply.github.com>
2022-05-10 23:59:29 +0100

    Slayer removed
    All slayer content removed

--- src/tasks/minions/monsterActivity.ts
+++ src/tasks/minions/monsterActivity.ts
@@ -1,145 +1,44 @@
 import { Task } from 'klasa';
-import { MonsterKillOptions, Monsters } from 'oldschooljs';
+import { MonsterKillOptions } from 'oldschooljs';
 
 import { PvMMethod } from '../../lib/constants';
-import { SlayerActivityConstants } from '../../lib/minions/data/combatConstants';
 import killableMonsters from '../../lib/minions/data/killableMonsters';
 import { addMonsterXP } from '../../lib/minions/functions';
 import announceLoot from '../../lib/minions/functions/announceLoot';
-import { prisma, trackLoot } from '../../lib/settings/prisma';
+import { trackLoot } from '../../lib/settings/prisma';
 import { runCommand } from '../../lib/settings/settings';
-import { UserSettings } from '../../lib/settings/types/UserSettings';
-import { SkillsEnum } from '../../lib/skilling/types';
-import { SlayerTaskUnlocksEnum } from '../../lib/slayer/slayerUnlocks';
-import { calculateSlayerPoints, getSlayerMasterOSJSbyID, getUsersCurrentSlayerInfo } from '../../lib/slayer/slayerUtil';
 import { MonsterActivityTaskOptions } from '../../lib/types/minions';
-import { roll } from '../../lib/util';
 import { handleTripFinish } from '../../lib/util/handleTripFinish';
 
 export default class extends Task {
 	async run(data: MonsterActivityTaskOptions) {
-		const { monsterID, userID, channelID, quantity, duration, usingCannon, cannonMulti, burstOrBarrage } = data;
+		const { monsterID, userID, channelID, quantity, duration } = data;
 		const monster = killableMonsters.find(mon => mon.id === monsterID)!;
 		const user = await this.client.fetchUser(userID);
 		await user.incrementMonsterScore(monsterID, quantity);
 
-		const usersTask = await getUsersCurrentSlayerInfo(user.id);
-		const isOnTask =
-			usersTask.assignedTask !== null &&
-			usersTask.currentTask !== null &&
-			usersTask.assignedTask.monsters.includes(monsterID);
-		const quantitySlayed = isOnTask ? Math.min(usersTask.currentTask!.quantity_remaining, quantity) : null;
-
-		const mySlayerUnlocks = user.settings.get(UserSettings.Slayer.SlayerUnlocks);
-
-		const slayerMaster = isOnTask ? getSlayerMasterOSJSbyID(usersTask.slayerMaster!.id) : undefined;
-		// Check if superiors unlock is purchased
-		const superiorsUnlocked = isOnTask
-			? mySlayerUnlocks.includes(SlayerTaskUnlocksEnum.BiggerAndBadder)
-			: undefined;
-
-		const superiorTable = superiorsUnlocked && monster.superior ? monster.superior : undefined;
-		const isInCatacombs = !usingCannon ? monster.existsInCatacombs ?? undefined : undefined;
-
 		const killOptions: MonsterKillOptions = {
-			onSlayerTask: isOnTask,
-			slayerMaster,
-			hasSuperiors: superiorTable,
-			inCatacombs: isInCatacombs
+			areaSkull: monster.areaSkull
 		};
 
-		// Calculate superiors and assign loot.
-		let newSuperiorCount = 0;
-		if (superiorTable && isOnTask) {
-			for (let i = 0; i < quantity; i++) if (roll(200)) newSuperiorCount++;
-		}
 		// Regular loot
-		const loot = monster.table.kill(quantity - newSuperiorCount, killOptions);
+		const loot = monster.table.kill(quantity, killOptions);
 		if (monster.specialLoot) {
 			monster.specialLoot(loot, user, data);
-		}
-		if (newSuperiorCount) {
-			// Superior loot and totems if in catacombs
-			loot.add(superiorTable!.kill(newSuperiorCount));
-			if (isInCatacombs) loot.add('Dark totem base', newSuperiorCount);
 		}
 
 		const xpRes = await addMonsterXP(user, {
 			monsterID,
 			quantity,
 			duration,
-			isOnTask,
-			taskQuantity: quantitySlayed,
-			minimal: true,
-			usingCannon,
-			cannonMulti,
-			burstOrBarrage,
-			superiorCount: newSuperiorCount
+			minimal: true
 		});
 
 		announceLoot({ user, monsterID: monster.id, loot, notifyDrops: monster.notifyDrops });
 
-		if (newSuperiorCount && newSuperiorCount > 0) {
-			const oldSuperiorCount = await user.settings.get(UserSettings.Slayer.SuperiorCount);
-			user.settings.update(UserSettings.Slayer.SuperiorCount, oldSuperiorCount + newSuperiorCount);
-		}
-		const superiorMessage = newSuperiorCount ? `, including **${newSuperiorCount} superiors**` : '';
 		let str =
-			`${user}, ${user.minionName} finished killing ${quantity} ${monster.name}${superiorMessage}.` +
+			`${user}, ${user.minionName} finished killing ${quantity} ${monster.name}.` +
 			` Your ${monster.name} KC is now ${user.getKC(monsterID)}.\n${xpRes}\n`;
-		if (
-			monster.id === Monsters.Unicorn.id &&
-			user.hasItemEquippedAnywhere('Iron dagger') &&
-			!user.hasItemEquippedOrInBank('Clue hunter cloak')
-		) {
-			loot.add('Clue hunter cloak');
-			loot.add('Clue hunter boots');
-
-			str += '\n\nWhile killing a Unicorn, you discover some strange clothing in the ground - you pick them up.';
-		}
-
-		let thisTripFinishesTask = false;
-
-		if (isOnTask) {
-			const effectiveSlayed =
-				monsterID === Monsters.KrilTsutsaroth.id &&
-				usersTask.currentTask!.monster_id !== Monsters.KrilTsutsaroth.id
-					? quantitySlayed! * 2
-					: monsterID === Monsters.Kreearra.id && usersTask.currentTask!.monster_id !== Monsters.Kreearra.id
-					? quantitySlayed! * 4
-					: monsterID === Monsters.GrotesqueGuardians.id &&
-					  user.settings.get(UserSettings.Slayer.SlayerUnlocks).includes(SlayerTaskUnlocksEnum.DoubleTrouble)
-					? quantitySlayed! * 2
-					: quantitySlayed!;
-
-			const quantityLeft = Math.max(0, usersTask.currentTask!.quantity_remaining - effectiveSlayed);
-
-			thisTripFinishesTask = quantityLeft === 0;
-			if (thisTripFinishesTask) {
-				const currentStreak = user.settings.get(UserSettings.Slayer.TaskStreak) + 1;
-				await user.settings.update(UserSettings.Slayer.TaskStreak, currentStreak);
-				const points = await calculateSlayerPoints(currentStreak, usersTask.slayerMaster!, user);
-				const newPoints = user.settings.get(UserSettings.Slayer.SlayerPoints) + points;
-				await user.settings.update(UserSettings.Slayer.SlayerPoints, newPoints);
-				str += `\n**You've completed ${currentStreak} tasks and received ${points} points; giving you a total of ${newPoints}; return to a Slayer master.**`;
-				if (usersTask.assignedTask?.isBoss) {
-					str += ` ${await user.addXP({ skillName: SkillsEnum.Slayer, amount: 5000, minimal: true })}`;
-					str += ' for completing your boss task.';
-				}
-			} else {
-				str += `\nYou killed ${effectiveSlayed}x of your ${
-					usersTask.currentTask!.quantity_remaining
-				} remaining kills, you now have ${quantityLeft} kills remaining.`;
-			}
-			await prisma.slayerTask.update({
-				where: {
-					id: usersTask.currentTask!.id
-				},
-				data: {
-					quantity_remaining: quantityLeft
-				}
-			});
-		}
 
 		const { previousCL, itemsAdded } = await user.addItemsToBank({ items: loot, collectionLog: true });
 
@@ -168,25 +67,20 @@ export default class extends Task {
 			user,
 			channelID,
 			str,
-			isOnTask && thisTripFinishesTask
-				? undefined
-				: res => {
-						user.log(`continued trip of killing ${monster.name}`);
-						let method: PvMMethod = 'none';
-						if (usingCannon) method = 'cannon';
-						else if (burstOrBarrage === SlayerActivityConstants.IceBarrage) method = 'barrage';
-						else if (burstOrBarrage === SlayerActivityConstants.IceBurst) method = 'burst';
-						return runCommand({
-							message: res,
-							commandName: 'k',
-							args: {
-								name: monster.name,
-								quantity,
-								method
-							},
-							isContinue: true
-						});
-				  },
+			res => {
+				user.log(`continued trip of killing ${monster.name}`);
+				let method: PvMMethod = 'none';
+				return runCommand({
+					message: res,
+					commandName: 'k',
+					args: {
+						name: monster.name,
+						quantity,
+						method
+					},
+					isContinue: true
+				});
+			},
 			image!,
 			data,
 			itemsAdded
